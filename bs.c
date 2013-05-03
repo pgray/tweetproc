72110b9881b84aaa018520a9de3e45cd5d4bbcfd
pg <pg@Think.(none)>
2013-05-03 16:26:44 -0400

    changes to bs

--- bs.c
+++ bs.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+
+#define NONCE_LENGTH 32
+void createnonce(char[NONCE_LENGTH]);
 
 int main(int argc, char **argv)
 {
-	char *front = "POST /1/statuses/update.json?include_entities=true HTTP/1.1\nAccept: */*\nConnection:close\n";
+	char *post = "POST /1/statuses/update.json?include_entities=true HTTP/1.1\nAccept: */*\nConnection:close\n";
 	char *useragent = "User-Agent: ";
 	char *contentType = "Content-Type: ";
 	char *auth = "Authorization: ";
@@ -11,9 +15,19 @@ int main(int argc, char **argv)
 	char *host = "Host: api.twitter.com";
 	char *status = "status=";
 
-
+	char nonce[NONCE_LENGTH];
+	createnonce(nonce);
 
 
 
 	return 0;
+}
+
+void createnonce(char nonce[NONCE_LENGTH])
+{
+	int i,j;
+	for(i=0;i<NONCE_LENGTH;i++)
+	{
+		j=random();
+	}
 }
