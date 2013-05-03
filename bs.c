be25ff8eeeb62b8951f8ca8a61c7ef6e2feb3d07
pg <pg@Think.(none)>
2013-05-03 15:57:37 -0400

    bs file for hashing out stuff

---
+++ bs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	char *front = "POST /1/statuses/update.json?include_entities=true HTTP/1.1\nAccept: */*\nConnection:close\n";
+	char *useragent = "User-Agent: ";
+	char *contentType = "Content-Type: ";
+	char *auth = "Authorization: ";
+	char *contentLength = "Content-Length: ":
+	char *host = "Host: api.twitter.com";
+	char *status = "status=";
+
+
+
+
+
+	return 0;
+}
--- tweetproc.c
+++ tweetproc.c
@@ -1,5 +1,9 @@
 /*
 	Main file to be compiled and run.
+	tweetproc takes in the username and password for a twitter account
+	and sends hourly statistic updates about the computer it is being run on
+
+	example: ./tweetproc username password
 */
 
 #include <stdio.h>
